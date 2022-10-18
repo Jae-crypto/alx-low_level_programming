5789c949593e9d960e5a3f17c3f0a09f38556650
Jae-crypto <okidi.jacob@gmail.com>
2022-10-18 11:35:43 -0700

    Commiting task0 again

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,31 +1,16 @@
-#include "main.h"
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-/**
- *main - Prints Holberton as a message.
- *
- *Return: Always 0 (Success)
- */
-int main(void)
-{
-int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
-int count, sz;
-sz = sizeof(str) / sizeof(int);
-for (count = 0; count < sz; count++)
-{
-_putchar(str[count]);
-}
-_putchar('\n');
-return (0);
-}
+#ifndef main_h
+#define main_h
+extern int _putchar(char c);
+extern void print_alphabet(void);
+extern void print_alphabet_x10(void);
+extern int _islower(int c);
+extern int _isalpha(int c);
+extern int print_sign(int n);
+extern int _abs(int);
+extern int print_last_digit(int);
+extern void jack_bauer(void);
+extern void times_table(void);
+extern int add(int, int);
+extern void print_to_98(int n);
+extern void print_times_table(int n);
+#endif
