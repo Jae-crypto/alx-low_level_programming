74a522fc191f1c486cdb9110ffc0315b95c4e616
Jae-crypto <okidi.jacob@gmail.com>
2022-10-24 17:07:50 -0700

    new commit

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,12 @@
 #include "main.h"
+
 /**
-*reset_to_98-update the value of n to 98
-*@n:input
+*reset_to_98 - update the value of n to 98
+*@n:data to change to 98
 *Return: n
 */
+
 void reset_to_98(int *n)
 {
-*n = 98;
+	*n = 98;
 }
