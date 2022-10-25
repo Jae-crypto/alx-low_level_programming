dfd349544b2c3dd7721b2543656c8d26043e97cf
Jae-crypto <okidi.jacob@gmail.com>
2022-10-24 18:15:36 -0700

    go

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,30 +1,27 @@
 #include "main.h"
 
 /**
- *  * puts_half - print the other half
- *   * @str:needed params
- *    * Return:0
- *     */
+ *puts_half - print the other half
+ *@str:needed params
+ *Return:0
+ */
 
 void puts_half(char *str)
 {
-		int i, j;
-
-			i = 0;
-				while (str[i])
-						{
-									i++;
-										}
-					i--;
-						j = i / 2;
-							i = 0;
-								while (str[i])
-										{
-													if (i > j)
-																{
-																				_putchar(str[i]);
-																						}
-															i++;
-																}
-									_putchar('\n');
+int i, j;
+i = 0;
+while (str[i])
+{
+i++;
+}
+i--;
+j = i / 2;
+i = 0;
+while (str[i])
+{
+if (i > j)
+{																				_putchar(str[i]);																		}
+i++;
+}
+_putchar('\n');
 }
