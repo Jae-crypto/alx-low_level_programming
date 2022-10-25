18ab9b76213c2cda2fdad07c15f28bc8a7445876
Jae-crypto <okidi.jacob@gmail.com>
2022-10-24 18:31:02 -0700

    hh

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,22 +1,21 @@
-#include "main.h"
+nclude "main.h"
 
 /**
- *  * _strcpy - copy a string
- *   * @dest: string to copy to
- *    * @src:source to copy from
- *     * Return:copy of src
- *      */
+ *_strcpy - copy a string
+ *@dest: string to copy to
+ *@src:source to copy from
+ *Return:copy of src
+ */
 
 char *_strcpy(char *dest, char *src)
 {
-		int i;
-
-			i = 0;
-				while (src[i])
-						{
-									dest[i] = src[i];
-											i++;
-												}
-					dest[i] = '\0';
-						return (dest);
+int i;
+i = 0;
+while (src[i])
+{
+dest[i] = src[i];
+i++;
+}
+dest[i] = '\0';
+return (dest);
 }
