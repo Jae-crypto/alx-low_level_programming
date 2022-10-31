d182da41caa51879d587ce0c911af1dd631cbaac
Jae-crypto <okidi.jacob@gmail.com>
2022-10-31 14:43:58 -0700

    task1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- *_memcpy - copies memory area
- *@dest: destination memory area
- *@src: source memory area
- *@n: bytes filled
- *Return: dest
- */
+*_memcpy - copies memory area
+*@dest: destination memory area
+*@src: source memory area
+*@n: bytes filled
+*Return: dest
+*/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 unsigned int i;
@@ -13,5 +13,3 @@ for (i = 0; i < n; i++)
 *(dest + i) = *(src + i);
 return (dest);
 }
-Footer
-
