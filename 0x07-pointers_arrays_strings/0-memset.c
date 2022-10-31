b83688e54b75f759cb0553bce23216fb8618ea08
Jae-crypto <okidi.jacob@gmail.com>
2022-10-31 12:08:43 -0700

    First commit

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include "main.h"
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ */
+
+
