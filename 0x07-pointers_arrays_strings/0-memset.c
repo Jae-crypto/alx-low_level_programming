d5f1e5115a0a64618af717c4bb3ea47e530bb0a2
Jae-crypto <okidi.jacob@gmail.com>
2022-10-31 12:53:00 -0700

    Task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,9 +1,15 @@
-#include<stdio.h>
 #include "main.h"
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ *_memset - fills memory with a byte
+ *@s: memory area
+ *@b: constant byte
+ * @n: bytes filled
+ *Return: dest
  */
-
-
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n; i++)
+*(s + i) = b;
+return (s);
+}
