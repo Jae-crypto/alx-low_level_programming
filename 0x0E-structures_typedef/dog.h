15208fef626e5f76c57c3c817f18c6b2f5bb2381
Jae-crypto <okidi.jacob@gmail.com>
2022-11-17 07:25:39 -0800

    task0 comit message

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,25 +1,10 @@
-#ifndef DOG_H_
-#define DOG_H_
-
 /**
- * dog_t - Typedef for struct dog
+ *_putchar - write the character c to stdout
+ *@c: the character to print
+ *Return: on the success
+ *on error, -1 is the returned and eror is set appropriately
  */
-typedef struct dog dog_t;
-
-/**
- *struct dog - struct dog
- *@name: name of the dog
- *@age: age of the dog
- *@owner: owner of the dog
- *Description: this struct is for dog
- */
-struct dog
+int _putchar(char c)
 {
-char *name;
-float age;
-char *owner;
-};
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d)
+	return (write(1, &c, 1));
+
