220d51acf5fcdbcb46e86e1a61c389185ea74bf5
Jae-crypto <okidi.jacob@gmail.com>
2022-11-17 10:41:05 -0800

    first commit message

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+*print_name_as_is: prints a name
+*@name: person's name
+*Return: Nothing.
+*/
+
+void print_name_as_is(char *name)
+{
+printf("Hello, I am called %s\n", name);
+}
+
+
