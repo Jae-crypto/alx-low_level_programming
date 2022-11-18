fa158bcce6e05c9eaa8a202bab6c0a22430be696
Jae-crypto <okidi.jacob@gmail.com>
2022-11-18 05:23:00 -0800

    comit message

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_POINTERS_
+#define FUNCTION_POINTERS_
+#include <stddef.h>
+int _putchar(char c);
+void print_name(char *name, void) (*f)
+void array_iterator(int *array, size_)
+int int_index(int *array, int size, i)
+
+#endif
