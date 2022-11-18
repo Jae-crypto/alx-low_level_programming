493e54e19f73fbe22e14345eb59daab29b593155
Jae-crypto <okidi.jacob@gmail.com>
2022-11-18 05:43:49 -0800

    mm

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,9 +1,10 @@
-#ifndef FUNCTION_POINTERS_
-#define FUNCTION_POINTERS_
-#include <stddef.h>
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stdio.h>
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 int _putchar(char c);
-void print_name(char *name, void) (*f)
-void array_iterator(int *array, size_)
-int int_index(int *array, int size, i)
 
 #endif
